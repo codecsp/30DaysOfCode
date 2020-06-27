a8a348f7ad5205715e2cd9f5e67717499a53bf30
codecsp <mr.soham143@gmail.com>
2020-06-27 12:58:38 +0530

    modified reverse linked lis

--- Day5/linked_list.cpp
+++ Day5/linked_list.cpp
@@ -1,82 +1,90 @@
-// Iterative C++ program to reverse
-// a linked list
-#include <iostream>
-using namespace std;
+// Reverse a singly linked list.
+// Example:
 
-/* Link list node */
-struct Node {
-	int data;
-	struct Node* next;
-	Node(int data)
-	{
-		this->data = data;
-		next = NULL;
-	}
-};
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
 
-struct LinkedList {
-	Node* head;
-	LinkedList()
-	{
-		head = NULL;
-	}
+// submitted at leetode:https://leetcode.com/problems/reverse-linked-list/
 
-	/* Function to reverse the linked list */
-	void reverse()
-	{
-		// Initialize current, previous and
-		// next pointers
-		Node* current = head;
-		Node *prev = NULL, *next = NULL;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode
+ {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 
-		while (current != NULL) {
-			// Store next
-			next = current->next;
-
-			// Reverse current node's pointer
-			current->next = prev;
-
-			// Move pointers one position ahead.
-			prev = current;
-			current = next;
-		}
-		head = prev;
-	}
-
-	/* Function to print linked list */
-	void print()
-	{
-		struct Node* temp = head;
-		while (temp != NULL) {
-			cout << temp->data << " ";
-			temp = temp->next;
-		}
-	}
-
-	void push(int data)
-	{
-		Node* temp = new Node(data);
-		temp->next = head;
-		head = temp;
-	}
-};
-
-/* Driver program to test above function*/
-int main()
+//iterative
+class Solution
 {
-	/* Start with the empty list */
-	LinkedList ll;
-	ll.push(20);
-	ll.push(4);
-	ll.push(15);
-	ll.push(85);
+public:
+	ListNode* reverseList(ListNode* head)
+	{
 
-	cout << "Given linked list\n";
-	ll.print();
+		ListNode* next = NULL;
+		ListNode* prev = NULL ;
+		ListNode* curr = head;
 
-	ll.reverse();
+		while (curr)
+		{
+			next = curr->next;
+			curr->next = prev;
+			prev = curr;
+			curr = next;
+		}
 
-	cout << "\nReversed Linked list \n";
-	ll.print();
-	return 0;
-}
+		return prev;
+	}
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//recursive
+
+class Solution
+{
+	ListNode* head1 = NULL;
+public:
+
+
+
+	void reverse(ListNode* prev, ListNode* curr)
+	{
+
+		if (!curr)
+			return;
+		if (!curr->next)
+		{
+			head1 = curr;
+			head1->next = prev;
+			return;
+		}
+
+		reverse(curr, curr->next);
+		curr->next = prev;
+		return ;
+	}
+
+
+	ListNode* reverseList(ListNode* head)
+	{
+		//head1 = head;
+		reverse(NULL, head);
+
+		return head1;
+	}
+};
