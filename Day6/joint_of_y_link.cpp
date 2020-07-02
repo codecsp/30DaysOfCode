5827143a698a9c8176b8d62f169081bf0a0ecf08
codecsp <mr.soham143@gmail.com>
2020-07-02 17:10:50 +0530

    added two new problems

---
+++ Day6/joint_of_y_link.cpp
@@ -0,0 +1,80 @@
+// Write a program to find the node at which the intersection of two singly linked lists begins.
+
+// For example, the following two linked lists:
+
+// Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+// Output: Reference of the node with value = 8
+// Input Explanation: The intersected nodes value is 8 (note that this must not be 0 if the two lists intersect).
+//  From the head of A, it reads as [4,1,8,4,5].
+//  From the head of B, it reads as [5,6,1,8,4,5].
+//  There are 2 nodes before the intersected node in A;
+//  There are 3 nodes before the intersected node in B.
+
+//  submitted at leetcode: https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+//  easy question :
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+	{
+		int l1 = 0, l2 = 0;
+		ListNode* tmp1 = headA;
+		ListNode*tmp2 = headB;
+		while (tmp1)
+		{
+			l1++;
+			tmp1 = tmp1->next;
+		}
+
+		while (tmp2)
+		{
+			l2++;
+			tmp2 = tmp2->next;
+		}
+
+		int diff = 0;
+		if (l1 < l2)
+		{
+			diff = l2 - l1;
+
+			while (diff--)
+			{
+				headB = headB->next;
+			}
+		}
+		else
+		{
+			diff = l1 - l2;
+			while (diff--)
+			{
+				headA = headA->next;
+			}
+		}
+
+		while (headA && headB)
+		{
+			if (headB == headA)
+			{
+				return headA;
+				break;
+			}
+			else
+			{
+				headA = headA->next;
+				headB = headB->next;
+			}
+		}
+		return NULL;
+	}
+};
---
+++ Day6/palindrome_linked_list.cpp
@@ -0,0 +1,81 @@
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+
+
+submitted at leetcode: https://leetcode.com/problems/palindrome-linked-list/
+
+reference video (Nick white): https://www.youtube.com/watch?v=wk4QsvwQwdQ
+
+
+		/**
+		 * Definition for singly-linked list.
+		 * struct ListNode {
+		 *     int val;
+		 *     ListNode *next;
+		 *     ListNode() : val(0), next(nullptr) {}
+		 *     ListNode(int x) : val(x), next(nullptr) {}
+		 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+		 * };
+		 */
+		class Solution
+	{
+	public:
+
+		ListNode * reverse(ListNode * head)
+		{
+			ListNode* prev = NULL;
+
+			while (head)
+			{
+				ListNode* next = head->next;
+				head->next = prev;
+				prev = head;
+				head = next;
+			}
+			return prev;
+		}
+
+
+		bool isPalindrome(ListNode* head)
+		{
+
+			ListNode* fast = head;
+			ListNode* slow = head;
+
+			while (fast != NULL && fast->next != NULL)
+			{
+				slow = slow->next;
+				fast = fast->next->next;
+			}
+
+			slow = reverse(slow);
+
+			//cout<<"slow: "<<slow->val<<endl;
+
+			fast = head;
+
+			while (slow)
+			{
+				//cout<<fast->val<<" : "<<slow->val<<endl;
+
+				if (fast->val != slow->val)
+				{
+					return false;
+				}
+				else
+				{
+					fast = fast->next;
+					slow = slow->next;
+				}
+			}
+			return true;
+		}
+	};
